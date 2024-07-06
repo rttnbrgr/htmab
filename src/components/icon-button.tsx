f281583fe7573dc04956a674a626c91196ca3d39
Nathan Rothenberger <nroth10@gmail.com>
2024-07-06 13:47:22 -0500

    add ghost btn styles

--- src/components/icon-button.tsx
+++ src/components/icon-button.tsx
@@ -2,14 +2,19 @@ import styles from "./icon-button.module.css";
 import { Icon, validIcons } from "./icon";
 import { cva, type VariantProps } from "class-variance-authority";
 
-const iconButtonClasses = cva([styles.reset, styles.base, styles.fill], {
+const iconButtonClasses = cva([styles.reset, styles.base], {
   variants: {
+    style: {
+      fill: styles.fill,
+      ghost: styles.ghost,
+    },
     size: {
       small: styles.small,
       base: styles.medium,
     },
   },
   defaultVariants: {
+    style: "fill",
     size: "base",
   },
 });
@@ -20,6 +25,7 @@ type IconButtonProps = VariantProps<typeof iconButtonClasses> & {
 };
 
 export const IconButton = ({
+  style = "fill",
   size = "base",
   disabled,
   name,
@@ -28,7 +34,7 @@ export const IconButton = ({
   const iconSize = size === "base" ? 20 : 12;
 
   return (
-    <button className={iconButtonClasses({ size })} disabled={disabled}>
+    <button className={iconButtonClasses({ style, size })} disabled={disabled}>
       <Icon name={name} size={iconSize} />
     </button>
   );
